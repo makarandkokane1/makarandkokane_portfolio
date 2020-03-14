44f0c670d39a234ea78de53e402091729897f37a
Makarand Kokane <makarand.kokane@gmail.com>
2020-03-14 21:30:30 +0530

    API to fetch an environment variable value for gitub project root path.

---
+++ utilities/mkEnvironmentVariables.cxx
@@ -0,0 +1,54 @@
+
+/*-------------------------------------------------------------------------------------------------
+
+14-mar-2020: Wrote an API to get env var value.
+
+-------------------------------------------------------------------------------------------------*/
+
+#include <cstdlib>
+#include <iostream>
+
+//-------------------------------------------------------------------------------------------------
+
+#include "mkEnvironmentVariables.h"
+
+//-------------------------------------------------------------------------------------------------
+
+const char* mkUtilities::getenv (const char* in_envVarName)
+{
+    return std::getenv (in_envVarName);
+}
+
+//-------------------------------------------------------------------------------------------------
+
+bool mkUtilities::getProjectRootPath (
+    const char*& out_varName,
+    const char*& out_value)
+{
+    out_varName = "MK_GH_ROOT";
+
+    out_value = mkUtilities::getenv (out_varName);
+
+    return (out_value != nullptr);
+}
+
+//-------------------------------------------------------------------------------------------------
+
+void mkUtilities::unittest_envVars ()
+{
+    const char* varName = nullptr;
+    const char* value = nullptr;
+
+    bool isDefined = mkUtilities::getProjectRootPath (varName, value);
+
+    if (isDefined)
+    {
+        std::cout << "\n" << varName << " = " << value << "\n";
+    }
+    else
+    {
+        std::cout << "\n" << varName << " not defined.\n";
+    }
+}
+
+//-------------------------------------------------------------------------------------------------
---
+++ utilities/unittests1/unittests1.cxx
@@ -0,0 +1,9 @@
+
+#include "../mkEnvironmentVariables.h"
+
+int main (int argc, char *argv [])
+{
+    mkUtilities::unittest_envVars ();
+
+    return 0;
+}
